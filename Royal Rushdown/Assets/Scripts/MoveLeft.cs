94a00a8a25b15ebd00982b14214fcaa02f3a140d
Alicia Lin <alicil3@uci.edu>
2017-10-27 21:49:26 -0700

    Enemy Spawns at origin

---
+++ Royal Rushdown/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MoveLeft : MonoBehaviour {
+	
+	public int speed = -5;
+	public int delete_at = 0;
+
+	void Start () {
+		
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(gameObject.transform.position.x < delete_at)
+		{
+			Destroy (gameObject);
+		}
+		else{
+
+			GetComponent<Rigidbody2D>().velocity = new Vector2(speed,0);
+		}
+
+
+	}
+}
---
+++ Royal Rushdown/Assets/Scripts/Spawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+	public GameObject spawnPrefab;
+	// Use this for initialization
+	void Start () {
+		SpawnMe ();
+	}
+
+	public void SpawnMe(){
+		Instantiate(spawnPrefab, transform.position, transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SpawnMe ();
+	}
+}
