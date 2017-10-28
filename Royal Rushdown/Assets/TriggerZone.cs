a88ab37b6fdf0fdcd51d686fd780f37cc3493213
Skammerman99 <specialksocray@gmail.com>
2017-10-27 21:49:13 -0700

    Enemy Disappears and moves
    Can disappear with trigger zone and cool shit like that

---
+++ Royal Rushdown/Assets/TriggerZone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
