33933be437c8293e54aedf496deeacc104dbad91
Kyle Wong <kylewonga@gmail.com>
2017-10-27 21:38:15 -0700

    Added DirectionalNotes and NoteManager

---
+++ Royal Rushdown/Assets/Scripts/DirectionalNote.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalNote : MonoBehaviour {
+
+    // Use this for initialization
+    public enum Direction
+    {
+        Up, Right, Down, Left
+    }
+    public Direction direction = Direction.Right;
+    public float heightOffset = 0;
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+    public void followEnemy(Transform enemyTransform, float offset)
+    {
+        transform.SetParent(enemyTransform);
+        heightOffset = offset;
+        transform.position = Vector3.up * heightOffset;
+    }
+    public void updateOffset(float newOffset)
+    {
+        heightOffset = newOffset;
+        transform.position = Vector3.up * heightOffset;
+
+
+    }
+    public int getDirection()
+    {
+        return (int)direction;
+    }
+    public void setDirection(Direction dir)
+    {
+        direction = dir;
+        switch (dir)
+        {
+            case (Direction.Right):
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                break;
+            case (Direction.Down):
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, -90));
+                break;
+            case (Direction.Left):
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
+                break;
+            case (Direction.Up):
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+                break;
+        }
+    }
+    public void setDirection(int dir)
+    {
+        direction = (Direction)dir;
+        switch (dir)
+        {
+            case (1):
+                transform.rotation = Quaternion.Euler(new Vector3(0,0,0));
+                break;
+            case (2):
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, -90));
+                break;
+            case (3):
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
+                break;
+            case (0):
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+                break;
+        }
+    }
+}
--- Royal Rushdown/Assets/Scripts/NoteManager.cs
+++ Royal Rushdown/Assets/Scripts/NoteManager.cs
@@ -5,12 +5,96 @@ using UnityEngine;
 public class NoteManager : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
+    public enum Direction
+    {
+        Up, Right, Down, Left
+    }
+    public List<GameObject> noteList;
+    public string prefabName;
+    public float listOffset;
+    public float noteOffset;
+    private void Awake()
+    {
+        noteList = new List<GameObject>();
+    }
+    void Start () {
+        addNote(Direction.Right);
+        addNote(Direction.Down);
+        addNote(Direction.Left);
+        addNote(Direction.Up);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            printNotes();
+            removeFront();
+            printNotes();
+        }
 	}
+    
+    public void addNote(int dir)
+    {
+        GameObject newNote = (GameObject)Instantiate(Resources.Load(prefabName));
+        newNote.GetComponent<DirectionalNote>().followEnemy(gameObject.transform,listOffset+noteOffset*noteList.Count);
+        newNote.GetComponent<DirectionalNote>().setDirection(dir);
+        noteList.Add(newNote);
+    }
+    public void addNote(Direction dir)
+    {
+        GameObject newNote = (GameObject)Instantiate(Resources.Load(prefabName));
+        newNote.GetComponent<DirectionalNote>().followEnemy(gameObject.transform, listOffset + noteOffset * noteList.Count);
+        newNote.GetComponent<DirectionalNote>().setDirection((int)dir);
+        noteList.Add(newNote);
+    }
+    public int removeFront()
+    {
+        if (!empty())
+        {
+            
+            int toReturn = getNextNoteDirection();
+            Destroy(noteList[0]);
+            noteList.RemoveAt(0);
+            for (int i = 0; i < noteList.Count; i++)
+            {
+                noteList[i].GetComponent<DirectionalNote>().updateOffset(listOffset + noteOffset * i);
+            }
+            
+            return toReturn;
+        }
+        else
+        {
+            return -1;      //empty list
+        }
+    }
+    public int noteListSize()
+    {
+        return noteList.Count;
+    }
+    public bool empty()
+    {
+        return noteList.Count == 0;
+    }
+    public int getNextNoteDirection()
+    {
+        if (!empty())
+        {
+            return (int)noteList[0].GetComponent<DirectionalNote>().getDirection();
+        } else
+        {
+            return -1;      //empty list
+        }
+    }
+    public void printNotes()
+    {
+        string result = "next->";
+        for(int i = 0; i < noteList.Count; i++)
+        {
+            result+=((Direction)noteList[i].GetComponent<DirectionalNote>().getDirection()) + "->";
+        }
+        result += "end";
+        print(result);
+    }
+    
 }
