a5ed152c2c6d2b40aba86a0056c15aa5909633c7
Kyle Wong <kylewonga@gmail.com>
2017-10-28 14:29:39 -0700

    Added boneExplosion prefab

---
+++ Royal Rushdown/Assets/Scripts/ParticleExplosion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleExplosion : MonoBehaviour {
+
+    // Use this for initialization
+    public int particlesEmitted;
+    public float lifeTime;
+	void Start () {
+        explode(15, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void explode(int numParticles, float objLifeTime)
+    {
+        GetComponent<ParticleSystem>().Emit(numParticles);
+        Destroy(gameObject, objLifeTime);
+    }
+}
