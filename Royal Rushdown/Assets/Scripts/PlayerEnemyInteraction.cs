28af99427683d3be21c69740c9809606651b962b
Alicia Lin <alicil3@uci.edu>
2017-10-28 15:47:23 -0700

    health system?

--- Royal Rushdown/Assets/Scripts/PlayerEnemyInteraction.cs
+++ Royal Rushdown/Assets/Scripts/PlayerEnemyInteraction.cs
@@ -3,19 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerEnemyInteraction : MonoBehaviour {
+	float health = 110;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
+	// Update is called once per frame;
 	void Update () {
-		
+		if (health < 100)
+		{
+			health = Mathf.Clamp (health + Time.deltaTime*5, 0, 100);
+		}
+
 	}
 		
 	void OnTriggerEnter2D(Collider2D other) {
-		Destroy (gameObject);
+		if (health == 110) {
+			health -= 50;
+		} else {
+			health -= (health * .3f);
+		}
+
+		if (health < 10) {
+			Destroy (gameObject);
+		} 
 	}
 
 }
