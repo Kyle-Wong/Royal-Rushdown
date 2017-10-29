48e3bf8756c390ccb459ff32813e950539a75d7a
Kyle Wong <kylewonga@gmail.com>
2017-10-29 13:04:13 -0700

    Added combo counter

---
+++ Royal Rushdown/Assets/Scripts/HitCounter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HitCounter : MonoBehaviour {
+
+    // Use this for initialization
+    public Color startColor;
+    public Color endColor;
+    public int endColorShiftAt = 100;
+    
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Text>().text = FightZone.hitCounter + "x";
+        if(FightZone.hitCounter%10 == 0)
+            GetComponent<Text>().color = Color.Lerp(startColor, endColor, FightZone.hitCounter*1f / endColorShiftAt);
+    }
+}
