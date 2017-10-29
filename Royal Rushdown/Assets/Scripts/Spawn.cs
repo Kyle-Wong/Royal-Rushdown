ea96b7786e860117c19358d89960b1c0163354d3
Alicia Lin <alicil3@uci.edu>
2017-10-28 21:14:15 -0700

    oops

--- Royal Rushdown/Assets/Scripts/Spawn.cs
+++ Royal Rushdown/Assets/Scripts/Spawn.cs
@@ -37,12 +37,12 @@ public class Spawn : MonoBehaviour {
         }
         enemyList.Add(newEnemy);
 =======
-//        int numNotes = (int)Mathf.Max(1,averageNotes*(1+-noteCountVariation + Random.value*noteCountVariation*2));
-//        int[] distinctNotesList = subList(new int[] { 0, 1, 2, 3 }, Mathf.Min(4,randFromAverage(NoteVariety)));
-//        for(int i = 0; i < numNotes; ++i)
-//        {
-//            newEnemy.GetComponent<NoteManager>().addNote(distinctNotesList[Random.Range(0, distinctNotesList.Length-1)]);
-//        }
+        int numNotes = (int)Mathf.Max(1,averageNotes*(1+-noteCountVariation + Random.value*noteCountVariation*2));
+        int[] distinctNotesList = subList(new int[] { 0, 1, 2, 3 }, Mathf.Min(4,randFromAverage(NoteVariety)));
+        for(int i = 0; i < numNotes; ++i)
+        {
+            newEnemy.GetComponent<NoteManager>().addNote(distinctNotesList[Random.Range(0, distinctNotesList.Length-1)]);
+        }
 >>>>>>> Stashed changes
     }
 	
@@ -61,29 +61,29 @@ public class Spawn : MonoBehaviour {
 		while (range > 0) {
 			range -= spawnTimeRangeDecrease;
 			spawnTime = min + Random.Range(0,range);
-//            averageNotes += averageNoteGrowth;
-//            NoteVariety += noteVarietyGrowth;
+            averageNotes += averageNoteGrowth;
+            NoteVariety += noteVarietyGrowth;
 			yield return new WaitForSeconds (3.33f);
 		}
 	}
-//    private int randFromAverage(float val)
-//    {
-//        int min = Mathf.FloorToInt(val);
-//        int max = Mathf.CeilToInt(val);
-//        return (Random.value > (val % 1)) ? min : max;
-//    }
-//    private int[] subList(int[] list, int newLength)
-//    {
-//        List<int> tempList = new List<int>(list);
-//        int[] result = new int[newLength];
-//        for(int i = 0; i < newLength; ++i)
-//        {
-//            int rng = Random.Range(0, tempList.Count);
-//            result[i] = tempList[rng];
-//
-//            tempList.RemoveAt(rng);
-//            
-//        }
-//        return result;
-//    }
+    private int randFromAverage(float val)
+    {
+        int min = Mathf.FloorToInt(val);
+        int max = Mathf.CeilToInt(val);
+        return (Random.value > (val % 1)) ? min : max;
+    }
+    private int[] subList(int[] list, int newLength)
+    {
+        List<int> tempList = new List<int>(list);
+        int[] result = new int[newLength];
+        for(int i = 0; i < newLength; ++i)
+        {
+            int rng = Random.Range(0, tempList.Count);
+            result[i] = tempList[rng];
+
+            tempList.RemoveAt(rng);
+            
+        }
+        return result;
+    }
 }
