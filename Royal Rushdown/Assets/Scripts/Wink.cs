d4fc89d0973aa57031af8819ebe9469161fb9489
Kyle Wong <kylewonga@gmail.com>
2017-10-29 22:31:58 -0700

    *WINK*

---
+++ Royal Rushdown/Assets/Scripts/Wink.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Wink : MonoBehaviour {
+
+    // Use this for initialization
+    public Sprite defaultSprite;
+    public Sprite activeSprite;
+    public float duration;
+    public float initialDelay;
+	void Start () {
+        StartCoroutine(winkAfterDelay());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private IEnumerator winkAfterDelay()
+    {
+        while (true)
+        {
+            GetComponent<Image>().sprite = defaultSprite;
+            yield return new WaitForSeconds(initialDelay);
+            GetComponent<Image>().sprite = activeSprite;
+            yield return new WaitForSeconds(duration);
+        }
+    }
+}
