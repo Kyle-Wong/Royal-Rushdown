ecf2d223d881ce26ae06f86063e2c9b232b48085
Kyle Wong <kylewonga@gmail.com>
2017-10-29 21:58:20 -0700

    Added character shadow

---
+++ Royal Rushdown/Assets/Scripts/ShadowPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowPlayer : MonoBehaviour {
+
+    // Use this for initialization
+    private SpriteRenderer targetRenderer;
+    private SpriteRenderer myRenderer;
+    private Animator myAnimator;
+    private Animator targetAnimator;
+	void Start () {
+        targetRenderer = transform.parent.GetComponent<SpriteRenderer>();
+        myRenderer = GetComponent<SpriteRenderer>();
+        myAnimator = GetComponent<Animator>();
+        targetAnimator = transform.parent.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(targetAnimator.enabled == true)
+        {
+            myAnimator.enabled = true;
+        } else
+        {
+            myAnimator.enabled = false;
+            myRenderer.sprite = targetRenderer.sprite;
+        }
+        
+	}
+}
