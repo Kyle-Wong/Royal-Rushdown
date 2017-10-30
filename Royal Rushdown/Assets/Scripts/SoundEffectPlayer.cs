ed3ba782ba33ba9ce7c77bd62e812032891b5618
Kyle Wong <kylewonga@gmail.com>
2017-10-29 18:50:49 -0700

    Added soundEffectPlayer script

---
+++ Royal Rushdown/Assets/Scripts/SoundEffectPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectPlayer : MonoBehaviour
+{
+
+    // Use this for initialization
+    public AudioClip[] clips;
+    private AudioSource source;
+    public bool allowRepeats = false;
+    private int lastPlayed = 0;
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void playRandomSound()
+    {
+        int index = 0;
+        while (clips.Length > 1)
+        {
+            index = Random.Range(0, clips.Length);
+            if (allowRepeats || index != lastPlayed)
+                break;
+        }
+        lastPlayed = index;
+        source.PlayOneShot(clips[index]);
+    }
+    public void playSound(int clipNum)
+    {
+        lastPlayed = clipNum;
+        source.PlayOneShot(clips[clipNum]);
+    }
+}
