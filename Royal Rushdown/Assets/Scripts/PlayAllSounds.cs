f1699ca3a97a701e479ed483e39acf126e472fd1
Kyle Wong <kylewonga@gmail.com>
2017-10-29 17:12:24 -0700

    Added more sound + some implemented

---
+++ Royal Rushdown/Assets/Scripts/PlayAllSounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAllSounds : MonoBehaviour {
+
+    // Use this for initialization
+    public AudioClip[] clipList;
+    private AudioSource source;
+	void Start () {
+        source = GetComponent<AudioSource>();
+		for(int i = 0; i < clipList.Length; ++i)
+        {
+            playClip(clipList[i]);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void playClip(AudioClip clip)
+    {
+        source.PlayOneShot(clip);
+    }
+    
+}
