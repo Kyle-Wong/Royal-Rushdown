2e3cd9a57be10475efec317622445f7dde07121b
Kyle Wong <kylewonga@gmail.com>
2017-10-29 21:16:27 -0700

    Added floating text

---
+++ Royal Rushdown/Assets/Scripts/FloatingText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingText : MonoBehaviour {
+
+    // Use this for initialization
+    public string[] wordList;
+    public bool useRandom = true;
+    private TextMesh myText;
+	void Start () {
+        myText = GetComponent<TextMesh>();
+        if (useRandom)
+        {
+            myText.text = wordList[Random.Range(0, wordList.Length)];
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
